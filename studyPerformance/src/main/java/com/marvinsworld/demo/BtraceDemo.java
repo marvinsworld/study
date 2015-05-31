ef8d325bb64264e3a49d76190ec4e96bebc89494
zhiqiang.ge <zhiqiang.ge@qunar.com>
2015-05-31 17:46:29 +0800

    '性能调优'

--- studyPerformance/src/main/java/com/marvinsworld/demo/BtraceDemo.java
+++ studyPerformance/src/main/java/com/marvinsworld/demo/BtraceDemo.java
@@ -1,8 +1,28 @@
 package com.marvinsworld.demo;
 
+import java.util.Random;
+
 /**
  * .
  * Created by zhiqiang.ge on 2015/5/31 16:36.
  */
 public class BtraceDemo {
+    public static void main(String[] args) {
+        //CaseObject object = new CaseObject();
+        while (true) {
+            Random random = new Random();
+            execute(random.nextInt(4000));
+            //object.execute(random.nextInt(4000));
+        }
+    }
+
+    public static Integer execute(int sleepTime) {
+        try {
+            Thread.sleep(sleepTime);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("sleep time is=>" + sleepTime);
+        return 0;
+    }
 }
--- studyPerformance/src/test/java/com/marvinsworld/demo/BtraceDemoTest.java
+++ studyPerformance/src/test/java/com/marvinsworld/demo/BtraceDemoTest.java
@@ -1,8 +1,33 @@
 package com.marvinsworld.demo;
 
+import com.sun.btrace.annotations.*;
+import static com.sun.btrace.BTraceUtils.*;
+
 /**
  * .
  * Created by zhiqiang.ge on 2015/5/31 16:36.
  */
+@BTrace
 public class BtraceDemoTest {
+    @TLS
+    private static long startTime = 0;
+
+    @OnMethod(clazz = "com.marvinsworld.demo.BtraceDemo", method = "execute")
+    public static void startMethod() {
+        startTime = timeMillis();
+    }
+
+    @OnMethod(clazz = "com.marvinsworld.demo.BtraceDemo", method = "execute", location = @Location(Kind.RETURN))
+    public static void endMethod() {
+        println(strcat("the class method execute time=>", str(timeMillis() - startTime)));
+        println("-------------------------------------------");
+    }
+
+    @OnMethod(clazz = "com.marvinsworld.demo.BtraceDemo", method = "execute", location = @Location(Kind.RETURN))
+    public static void traceExecute(@ProbeClassName String name, @ProbeMethodName String method, int sleepTime) {
+        println(strcat("the class name=>", name));
+        println(strcat("the class method=>", method));
+        println(strcat("the class method params=>", str(sleepTime)));
+
+    }
 }
