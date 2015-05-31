ab6c71d79b0666ecbfad999b1b4860153c488597
marvinsworld <marvinsworld@126.com>
2015-05-31 11:36:57 +0800

    'addtest'

---
+++ studyConcurrency/src/main/java/com/marvinsworld/demo/Demo.java
@@ -0,0 +1,11 @@
+package com.marvinsworld.demo;
+
+/**
+ * Description:
+ *
+ * @author Eason
+ * @since 15-3-1 上午10:43
+ */
+public class Demo {
+
+}
---
+++ studyZookeeper/src/main/java/com/marvinsworld/demo/curator/CreateNode.java
@@ -0,0 +1,29 @@
+package com.marvinsworld.demo.curator;
+
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.zookeeper.CreateMode;
+
+/**
+ * Description:
+ *
+ * @author Eason
+ * @since 15-5-26 上午5:56
+ */
+public class CreateNode {
+
+    static CuratorFramework client = CuratorFrameworkFactory.builder()
+            .connectString("192.168.8.3:2181,192.168.8.3:2182,192.168.8.3:2183")
+            .sessionTimeoutMs(5000)
+            .retryPolicy(new ExponentialBackoffRetry(1000, 3))
+            .build();
+
+    public static void main(String[] args) throws Exception {
+        client.start();
+        client.create()
+                .creatingParentsIfNeeded()
+                .withMode(CreateMode.EPHEMERAL)
+                .forPath("/zk-book/c1", "init".getBytes());
+    }
+}
